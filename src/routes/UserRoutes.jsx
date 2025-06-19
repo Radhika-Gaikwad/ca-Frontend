3cd3a4f43aa86ad359b50227565eb8dc1760c272
Mukilan K <mukilankumar003@gmail.com>
2025-06-19 18:13:38 +0530

    fix: correct import path for Home component

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home";
+import Home from "../pages/Home";
 import Login from "../features/authentication/LoginForm";
 import SignUpForm from "../features/authentication/SignUpForm";
 import Address from "../features/address/Address";
