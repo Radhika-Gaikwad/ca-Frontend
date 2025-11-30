1b7b2bdabd41a0a9a26a0d0f49958bb6c755b0e6
Gaikwad Radhika <139673080+Radhika-Gaikwad@users.noreply.github.com>
2025-11-30 23:09:41 +0530

    Update axiosInstance.js

--- src/utils/axios/axiosInstance.js
+++ src/utils/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://ca-y12j.onrender.com//api/",
+  baseURL: "https://ca-y12j.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
